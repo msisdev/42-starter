c780ee51d42d52294bbf33515f1569ba1c8e6a56
msisdev <msisdev@gmail.com>
2025-10-18 19:12:54 +0900

    add warning

--- ex00/src/global.c
+++ ex00/src/global.c
@@ -3,11 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   global.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minseobk <minseobk@student.42.fr>          +#+  +:+       +#+        */
+/*   By: minseobk <minseobk@student.42gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 16:24:36 by minseobk          #+#    #+#             */
-/*   Updated: 2025/08/04 16:24:41 by minseobk         ###   ########.fr       */
+/*   Updated: 2025/10/18 19:12:41 by minseobk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// USING GLOBAL VARIABLE WITHOUT STATIC QUALIFIER
+// VIOLATES NORMINETTE !!
 int	g_int;
