5cd0abc53a0390fde7bf94ddcd7d55ff2e6bc283
msisdev <msisdev@gmail.com>
2026-01-02 14:40:44 +0900

    add vscode config

--- exlibmlx/app/main.c
+++ exlibmlx/app/main.c
@@ -6,19 +6,19 @@
 /*   By: minseobk <minseobk@student.42gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 19:05:19 by minseobk          #+#    #+#             */
-/*   Updated: 2025/12/27 20:12:40 by minseobk         ###   ########.fr       */
+/*   Updated: 2026/01/02 14:40:18 by minseobk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-int main(void)
+int	main(void)
 {
-    void    *mlx_ptr;
-    void    *win_ptr;
+	void	*mlx_ptr;
+	void	*win_ptr;
 
-    mlx_ptr = mlx_init();
-    win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Hello 42!");
-    mlx_loop(mlx_ptr);
-    return (0);
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Hello 42!");
+	mlx_loop(mlx_ptr);
+	return (0);
 }
